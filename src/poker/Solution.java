58acf96c935c5e7f671e4ad10d342e1a8572ec5f
jrdanfox <fox.1061@osu.edu>
2019-06-06 15:45:24 -0400

    added i/o

--- src/poker/Solution.java
+++ src/poker/Solution.java
@@ -1,9 +1,50 @@
 package poker;
 
-public class Solution {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		Card card = new Card('A', 'S');
-		System.out.println(card.getSuit());
+public class Solution {
+	
+	// Deal a hand of 5 cards
+	public static Card[] dealHand(String[] cardStrings) {
+		Card[] hand = new Card[5];
+		for (int i = 0; i < cardStrings.length; i++) {
+			hand[i] = new Card(cardStrings[i].charAt(0), cardStrings[i].charAt(1));
+			System.out.print(Character.toString(hand[i].getValue()) + Character.toString(hand[i].getSuit()) + " ");
+		}
+		System.out.print("\n");
+		return hand;
+	}
+
+	public static void main(String[] args) throws FileNotFoundException {
+		// file io stuff
+		File file = new File("src/input");
+		Scanner scanner = new Scanner(file);
+		
+		String[] handStrings = new String[5];
+		int cardCount = 0;
+		int handCount = 0;
+		
+		Card[] blackHand, whiteHand;
+		
+		while (scanner.hasNext()) {
+			String next = scanner.next();
+			if (!next.equals("Black:") && !next.equals("White:")) { // filter out hand labels
+				handStrings[cardCount] = next;
+				cardCount++;
+				if (cardCount == 5) {
+					if (handCount == 0) { // deal black a hand
+						blackHand = dealHand(handStrings);
+						handCount++;
+					} else { // deal white a hand, reset handCount
+						whiteHand = dealHand(handStrings);
+						handCount = 0;
+					}
+					cardCount = 0;
+				}
+			}
+		}
+		scanner.close();
 	}
 }
